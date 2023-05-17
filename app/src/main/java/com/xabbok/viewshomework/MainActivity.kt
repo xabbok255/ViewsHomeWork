8afefbf009a0d1ac303be85c6aecfe847aae0696
xabbok255 <xabbok255@ya.ru>
2023-05-17 11:30:04 +0300

    Initial commit

---
+++ app/src/main/java/com/xabbok/viewshomework/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.xabbok.viewshomework
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.xabbok.viewshomework.ui.StatsView
+
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val statsView = findViewById<StatsView>(R.id.statsView)
+
+        statsView.data = listOf(0.25f, 0.25f, 0.25f, 0.25f)
+    }
+}
---
+++ app/src/main/java/com/xabbok/viewshomework/ui/StatsView.kt
@@ -0,0 +1,106 @@
+package com.xabbok.viewshomework.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.withStyledAttributes
+import com.xabbok.viewshomework.R
+import kotlin.math.ceil
+import kotlin.math.min
+import kotlin.random.Random
+
+class StatsView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : View(context, attributeSet, defStyleAttr, defStyleRes) {
+
+    private var textSize = AndroidUtils.dp(context, 20F)
+    private var lineWidth = AndroidUtils.dp(context, 5F)
+    private var colors = emptyList<Int>()
+
+    init {
+        context.withStyledAttributes(attributeSet, R.styleable.StatsView) {
+            textSize = getDimension(R.styleable.StatsView_textSize, textSize)
+            lineWidth = getDimension(R.styleable.StatsView_lineWidth, lineWidth)
+            colors = listOf(
+                getColor(R.styleable.StatsView_color1, generateRandomColor()),
+                getColor(R.styleable.StatsView_color2, generateRandomColor()),
+                getColor(R.styleable.StatsView_color3, generateRandomColor()),
+                getColor(R.styleable.StatsView_color4, generateRandomColor())
+            )
+        }
+    }
+
+    private var radius = 0F
+    private var center = PointF()
+    private var oval = RectF()
+    private val paint = Paint(
+        Paint.ANTI_ALIAS_FLAG
+    ).apply {
+        strokeWidth = this@StatsView.lineWidth
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+    }
+
+    private val textPaint = Paint(
+        Paint.ANTI_ALIAS_FLAG
+    ).apply {
+        textSize = this@StatsView.textSize
+        textAlign = Paint.Align.CENTER
+        style = Paint.Style.FILL
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+    }
+
+    var data: List<Float> = emptyList()
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = min(w, h) / 2F - lineWidth
+        center = PointF(w / 2F, h / 2F)
+        oval = RectF(
+            center.x - radius,
+            center.y - radius,
+            center.x + radius,
+            center.y + radius
+        )
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        if (data.isEmpty())
+            return
+
+        var startAngle = -90F
+        data.forEachIndexed { index, data ->
+            var angle = data * 360F
+            paint.color = colors.getOrElse(index) { generateRandomColor() }
+
+            canvas.drawArc(oval, startAngle, angle, false, paint)
+            startAngle += angle
+        }
+
+        canvas.drawText(
+            "%.2f%%".format(data.sum() * 100),
+            center.x,
+            center.y + textPaint.textSize / 4,
+            textPaint
+        )
+    }
+
+    private fun generateRandomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
+}
+
+object AndroidUtils {
+    fun dp(context: Context, dp: Float): Float =
+        ceil(context.resources.displayMetrics.density * dp)
+}
