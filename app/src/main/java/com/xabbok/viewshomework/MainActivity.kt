e9a93d90661016de45acac0b9d5a951abba295ab
xabbok255 <xabbok255@yandex.ru>
2023-05-17 16:03:37 +0300

    Задача 3. Not Filled*

--- app/src/main/java/com/xabbok/viewshomework/MainActivity.kt
+++ app/src/main/java/com/xabbok/viewshomework/MainActivity.kt
@@ -2,7 +2,10 @@ package com.xabbok.viewshomework
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.xabbok.viewshomework.ui.StatsView
+import com.xabbok.viewshomework.ui.StatsViewData
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity(R.layout.main_activity) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +13,26 @@ class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
         val statsView = findViewById<StatsView>(R.id.statsView)
 
-        statsView.data = listOf(500f, 500f, 500f, 500f)
+        //проверка freePercent от 1 до 100
+        lifecycleScope.launchWhenCreated {
+            var napr = 1
+            var perc = 0
+
+            while (true) {
+                statsView.data = StatsViewData(
+                    if (napr > 0) perc.toFloat() else 100f - perc,
+                    listOf(500f, 500f, 500f, 500f)
+                )
+
+                perc += 1
+                if (perc > 100) {
+                    perc = 0
+                    napr *= -1
+                    delay(100)
+                }
+
+                delay(5)
+            }
+        }
     }
 }
